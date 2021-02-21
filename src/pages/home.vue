7fa70d95cd41dd5960e2c6a55b433f778649e9c5
BaldCloakMan <540002338@qq.com>
2021-02-21 13:44:58 +0800

    目录构架

---
+++ src/pages/home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <nav-header/>
+        <router-view/>
+        <nav-footer/>
+    </div>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader.vue';
+export default {
+  components: { NavHeader },
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
